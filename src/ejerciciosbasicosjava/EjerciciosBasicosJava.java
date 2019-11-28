da0890c4fa4bad3e64978675d0252d8b14e8294a
Ruben <Ruben@DESKTOP-2EIGM9G>
2019-11-28 16:53:46 +0100

    Versión inicial con el primer ejercicio hecho

---
+++ src/ejerciciosbasicosjava/EjerciciosBasicosJava.java
@@ -0,0 +1,31 @@
+
+package ejerciciosbasicosjava;
+
+/**
+ *
+ * @author Ruben Juarez
+ */
+public class EjerciciosBasicosJava {
+    
+    public boolean fiestaArdillas(int numBellotas,boolean finDeSemana){
+        if(finDeSemana){
+        return true;
+        }
+    if(numBellotas>=40&& numBellotas<=60){
+    return true;
+    }
+    return false;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        EjerciciosBasicosJava ejercicios = new EjerciciosBasicosJava();
+        System.out.print (ejercicios.fiestaArdillas(30, false));
+        System.out.print (ejercicios.fiestaArdillas(50, true));
+        System.out.print (ejercicios.fiestaArdillas(70, true));
+        
+    }
+    
+}
